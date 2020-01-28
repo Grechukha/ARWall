a0804d1fb995ebfd82e45c4e468e1cd8f56494b8
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2020-01-28 23:35:18 +0200

    Добавил изменение цвета стены

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,5 +1,25 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class Wall : MonoBehaviour
 {
+    private MeshRenderer _meshRenderer;
+    private float _colorMaxIntensityLength = 3;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        _meshRenderer.material.color = GetNewColor();
+    }
+
+    private Color GetNewColor()
+    {
+        float colorIntensity = 1 - ((transform.localScale.z - 1) / _colorMaxIntensityLength);
+
+        return new Color(1, colorIntensity, colorIntensity);
+    }
 }
